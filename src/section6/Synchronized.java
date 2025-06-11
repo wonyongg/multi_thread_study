168733f9fee53c3ded2195794e9f58a6c6ff6331
wonyongg <111210881+wonyongg@users.noreply.github.com>
2025-06-12 02:24:28 +0900

    스레드 section6 임계 영역과 synchronized

---
+++ src/section6/Synchronized.java
@@ -0,0 +1,61 @@
+package section6;
+
+public class Synchronized {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+//        incrementingThread.join(); // 증가 스레드를 끝낼 때까지 메인스레드가 멈춰있기 때문에 충돌 발생 안함
+        decrementingThread.start();
+
+        incrementingThread.join(); // 개별적으로 두 개의 스레드가 움직이기 때문에 충돌로 데이터 정합성 문제 발생
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        public synchronized void increment() {items++;}
+        public synchronized void decrement() {items--;}
+
+        public synchronized int getItems() {
+            return items;
+        }
+    }
+}
---
+++ src/section6/SynchronizedLock.java
@@ -0,0 +1,73 @@
+package section6;
+
+public class SynchronizedLock {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+//        incrementingThread.join(); // 증가 스레드를 끝낼 때까지 메인스레드가 멈춰있기 때문에 충돌 발생 안함
+        decrementingThread.start();
+
+        incrementingThread.join(); // 개별적으로 두 개의 스레드가 움직이기 때문에 충돌로 데이터 정합성 문제 발생
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class DecrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+
+        Object lock = new Object();
+
+        public void increment() {
+            synchronized (this.lock) {
+                items++;
+            }
+        }
+        public void decrement() {
+            synchronized (this.lock) {
+                items--;
+            }
+        }
+
+        public int getItems() {
+            synchronized (this.lock) {
+                return items;
+            }
+        }
+    }
+}
